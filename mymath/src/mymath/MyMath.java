2a4c1e6c6b6ba89ccacb353cbb8abcf59427693a
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2023-02-14 10:37:19 +0100

    Rename sqrt -> mymath

--- mymath/src/mymath/MyMath.java
+++ mymath/src/mymath/MyMath.java
@@ -1,4 +1,4 @@
-package sqrt;
+package mymath;
 
 import java.util.Arrays;
 import java.util.stream.IntStream;
--- mymath/src/mymath/MyMathTest.java
+++ mymath/src/mymath/MyMathTest.java
@@ -1,4 +1,4 @@
-package sqrt;
+package mymath;
 
 import static org.junit.jupiter.api.Assertions.*;
 
